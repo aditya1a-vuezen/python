55db3b33f53e348cd7c58be39642140cd4e9e9c1
aditya1a-vuezen <adityavane648@gmail.com>
2026-05-05 16:23:19 +0530

    movement and pattern functions
    Implement movement functions for navigating a grid.

---
+++ DAY6/hurdle1.py
@@ -0,0 +1,39 @@
+def  right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def down():
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    move()
+def movement():
+    turn_left()
+    move()
+    right()
+    move()
+    down()
+    turn_left()
+    move()
+def pattern():
+    right()
+    move()
+    down()
+    movement()
+   
+move()
+movement()
+pattern()
+pattern()
+right()
+move()
+right()
+move()
+
+
+
+
+
